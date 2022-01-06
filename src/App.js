ccfebbfc5ca5afbb2297c3d084f6835ab4e39e53
hanifjumat <hanif.jumat@live.com>
2022-01-06 23:18:19 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. Hanif was here
+          Edit <code>src/App.js</code> and save to reload. Hanif washere made a changes here
         </p>
         <a
           className="App-link"
